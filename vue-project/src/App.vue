067a14f7afeac9e0fdfc2f647f5d0ec0d34b9a61
Diarrisso <diarrisso49@gmail.com>
2024-04-05 11:00:02 +0200

    add reactivite

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,13 +6,25 @@
 
 <script setup>
 import HelloWold from "@/components/HelloWorld.vue";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
-
+// ref props
 const user = ref({
   name: "Masingacite",
   age: 38,
 });
+
+const users = reactive({
+  name: "Masingacite",
+  age: 38,
+});
+
+// Reactivite
+const displayName = () => {
+  console.log("name", users.name, "age", users.age);
+};
+
+displayName();
 </script>
 
 <style scoped></style>
