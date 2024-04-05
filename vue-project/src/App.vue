a303d74f98d5e4285f6f530097be77a6151deae3
Diarrisso <diarrisso49@gmail.com>
2024-04-05 10:54:48 +0200

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWold :user="user" />
+  <input type="text" v-model="user.name" />
+  <input type="text" v-model="user.age" />
+</template>
+
+<script setup>
+import HelloWold from "@/components/HelloWorld.vue";
+import { ref } from "vue";
+
+
+const user = ref({
+  name: "Masingacite",
+  age: 38,
+});
+</script>
+
+<style scoped></style>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hallo</h1>
+
+    <p>{{ props.user.name }} {{ props.user.age}}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["user"]);
+</script>
+
+<style scoped></style>
